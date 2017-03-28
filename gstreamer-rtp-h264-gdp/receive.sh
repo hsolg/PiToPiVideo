4d6a26a78ecd56fa8b484efbf63ffd64049bf3af
Henrik Solgaard <h@solg.no>
2017-03-28 22:48:21 +0000

    Added a comment about sinks.

--- gstreamer-rtp-h264-gdp/receive.sh
+++ gstreamer-rtp-h264-gdp/receive.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-# Works in Ubuntu
+# Works in Raspbian and Ubuntu.
+# Use ximagesink with X and fbdevsink without X.
 gst-launch-1.0 -v tcpclientsrc host=$SRC_IP port=2222 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! ximagesink
