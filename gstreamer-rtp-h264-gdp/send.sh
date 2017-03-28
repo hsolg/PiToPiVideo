eaac3d14422f8125d69128dd1ce9d02b76148462
Henrik Solgaard <h@solg.no>
2017-03-28 23:07:09 +0000

    Reordered width and height parameters.

--- gstreamer-rtp-h264-gdp/send.sh
+++ gstreamer-rtp-h264-gdp/send.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-raspivid -n -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=$SRC_IP port=2222
+raspivid -n -t 0 -w 1080 -h 720 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=$SRC_IP port=2222
