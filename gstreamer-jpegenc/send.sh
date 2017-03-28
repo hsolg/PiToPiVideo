689cb85b45301a0756379f3a5a31c63fc6b15a43
Henrik Solgaard <h@solg.no>
2017-03-28 22:57:58 +0200

    Disabled preview in all send scripts using raspivid.

--- gstreamer-jpegenc/send.sh
+++ gstreamer-jpegenc/send.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-raspivid -t 0 -w 640 -h 480 -hf -ih -fps 10 -o - | gst-launch-1.0 -v fdsrc ! videoconvert ! jpegenc ! tcpserversink host=$SRC_IP port=2222
+raspivid -n -t 0 -w 640 -h 480 -hf -ih -fps 10 -o - | gst-launch-1.0 -v fdsrc ! videoconvert ! jpegenc ! tcpserversink host=$SRC_IP port=2222
--- nc/send.sh
+++ nc/send.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-raspivid -t 0 -w 1280 -h 720 -hf -ih -fps 20 -o - | nc -k -l 2222
+raspivid -n -t 0 -w 1280 -h 720 -hf -ih -fps 20 -o - | nc -k -l 2222
